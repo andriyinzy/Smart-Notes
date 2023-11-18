32b063246c4f197e8acbf5b425b68b84bf33e9cb
andriyinzy <143886512+andriyinzy@users.noreply.github.com>
2023-11-18 23:15:02 +0200

    Add files via upload

--- notes_main.py
+++ notes_main.py
@@ -3,7 +3,19 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QListWidget,
                              QLayout, QLabel, QVBoxLayout,QHBoxLayout, QTextEdit, 
                              QLineEdit, QInputDialog)
+import json
+
 app = QApplication([])
+
+# notes = {
+#     'Welcome' : {
+#        'text' : 'This is the best app for notes',
+#        'tags' : ['instruction', 'about']
+#    }
+# }
+
+# with open('notes_data.json', 'w') as file:
+#    json.dump(notes, file)
 
 win = QWidget()
 win.setWindowTitle('Smart notes')
@@ -52,8 +64,59 @@ col_2.addLayout(row_4)
 layout.addLayout(col_1, stretch = 2)
 layout.addLayout(col_2, stretch = 1)
 win.setLayout(layout)
+
+def show_note():
+    key = list_notes.selectedItems()[0].text()
+    print(key)
+    text_edit.setText(notes[key]['text'])
+    list_tags.clear()
+    list_tags.addItems(notes[key]['tags'])
+
+def add_note():
+    note_name, ok = QInputDialog.getText(win, 'Add note', 'Note name')
+    if ok and note_name !='':
+        notes[note_name] = {'text': '', 'tags':[]}
+        list_notes.addItem(note_name)
+        list_tags.addItems(notes[note_name]['tags'])
+
+def save_note():
+    if list_notes.selectedItems():
+        key = list_notes.selectedItems()[0].text()
+        notes[key]['text'] = text_edit.toPlainText()
+        with open('notes_data.json', 'w') as file:
+            json.dump(notes,file,sort_keys=True,ensure_ascii=False)
+    else:
+        print('Select note for save')
+
+def del_note():
+    if list_notes.selectedItems():
+        key = list_notes.selectedItems()[0].text()
+        del notes[key]
+        list_notes.clear()
+        list_tags.clear()
+        text_edit.clear()
+        list_notes.addItems(notes)
+        with open('notes_data.json', 'w') as file:
+            json.dump(notes,file,sort_keys=True,ensure_ascii=False)
+    else:
+        print('Select note for delete')
+
+
+
+
+
+btn_note_create.clicked.connect(add_note)
+list_notes.itemClicked.connect(show_note)
+btn_note_save.clicked.connect(save_note)
+btn_note_del.clicked.connect(del_note)
+
+
+
 win.show()
-app.exec()
+with open('notes_data.json', 'r') as file:
+    notes = json.load(file)
+list_notes.addItems(notes)
+app.exec_()
 
 
 
